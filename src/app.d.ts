126909299cf040d167182b62e9ea71553f2b5875
Isaac Harris-Holt <isaac@harris-holt.com>
2023-07-04 22:11:01 +0100

    setup vercel and meta component

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,10 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+import type { PageMeta } from '$lib/components/Meta.svelte';
+
 declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+    // interface Locals {}
+    interface PageData {
+        meta: PageMeta;
+    }
+    // interface Error {}
+    // interface Platform {}
 }
---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,29 @@
+<script lang="ts" context="module">
+    export type PageMeta = {
+        title?: string
+        description?: string
+    }
+</script>
+
+<script lang="ts">
+    import { page } from '$app/stores'
+    import { PUBLIC_DEPLOYMENT_ENV } from '$env/static/public'
+
+    export let defaultMeta: NonNullable<PageMeta>
+
+    let meta: PageMeta = defaultMeta
+    $: {
+        meta = {
+            ...defaultMeta,
+            ...$page.data.meta,
+        }
+        if (PUBLIC_DEPLOYMENT_ENV !== 'prod') {
+            meta.title = `${PUBLIC_DEPLOYMENT_ENV.toUpperCase()}: ${meta.title}`
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>{meta.title}</title>
+    <meta name="description" content={meta.description}/>
+</svelte:head>
