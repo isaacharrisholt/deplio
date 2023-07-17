93992166e915f56de6568ff5439014fca0a9bdbd
Isaac Harris-Holt <isaac@harris-holt.com>
2023-07-17 21:25:34 +0300

    add theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+    import '@fontsource-variable/space-grotesk'
     // The ordering of these imports is critical to your app working properly
-    import '@skeletonlabs/skeleton/themes/theme-rocket.css'
+    import '../theme.postcss'
     // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
     import '@skeletonlabs/skeleton/styles/skeleton.css'
     // Most of your app wide CSS should be put in this file
@@ -33,3 +34,29 @@
 />
 <Toast position="br" />
 <slot />
+
+<style lang="postcss">
+    :global(h1) {
+        @apply h1
+    }
+
+    :global(h2) {
+        @apply h2
+    }
+
+    :global(h3) {
+        @apply h3
+    }
+
+    :global(h4) {
+        @apply h4
+    }
+
+    :global(h5) {
+        @apply h5
+    }
+
+    :global(h6) {
+        @apply h6
+    }
+</style>
