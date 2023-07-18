a502b4aa89dbef11006f387df09def95a04c2c76
Isaac Harris-Holt <isaac@harris-holt.com>
2023-07-18 19:59:24 +0300

    add error icon to ErrorText.svelte

--- src/lib/components/forms/ErrorText.svelte
+++ src/lib/components/forms/ErrorText.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+    import { AlertCircle } from 'lucide-svelte'
+
     /** ID of the _parent_ component */
     export let id: string
     export let errors: string[] = []
 </script>
 
 {#if errors?.length}
-    <p class="variant-filled-error p-2" id="{id}-error">{errors.join(', ')}</p>
+    <div
+        class="variant-filled-error flex w-full max-w-full flex-row items-center gap-2 p-2 text-sm"
+    >
+        <AlertCircle size={16} />
+        <p id="{id}-error">{errors.join(', ')}</p>
+    </div>
 {/if}
