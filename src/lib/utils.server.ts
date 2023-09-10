c2e2b7ce6b8e2a25bd3de25731c6c127eb2d8087
Isaac Harris-Holt <isaac@harris-holt.com>
2023-09-10 13:58:19 +0100

    add webhook logic

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -1,7 +1,18 @@
-import { PUBLIC_DEPLOYMENT_ENV } from '$env/static/public'
+import { PUBLIC_DEPLOYMENT_ENV, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { VERCEL_URL } from '$env/static/private'
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from './types/database'
+import { PRIVATE_SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private'
 
-export const getSiteUrl = () => {
+export function getSiteUrl() {
     const protocol = PUBLIC_DEPLOYMENT_ENV === 'local' ? 'http' : 'https'
     return `${protocol}://${VERCEL_URL}`
+}
+
+export function getSupabaseAdminClient() {
+    return createClient<Database>(
+        PUBLIC_SUPABASE_URL,
+        PRIVATE_SUPABASE_SERVICE_ROLE_KEY,
+        { auth: { persistSession: false } },
+    )
 }
--- src/routes/api/webhook/+server.ts
+++ src/routes/api/webhook/+server.ts
@@ -1,6 +1,48 @@
-import type { RequestHandler } from '@sveltejs/kit'
+import { getSupabaseAdminClient } from '$lib/utils.server'
+import { error, type RequestHandler } from '@sveltejs/kit'
 
 export const POST: RequestHandler = async ({ request }) => {
-    console.log(await request.json())
+    const requestJson = await request.json()
+
+    const supabase = getSupabaseAdminClient()
+
+    // Check if this repo is registered to any projects
+    const { data: repo, error: repoFetchError } = await supabase
+        .from('repo')
+        .select('id, project (*)')
+        .eq('full_name', requestJson.repository.full_name)
+        .maybeSingle()
+
+    if (repoFetchError) {
+        console.log(`Error fetching repo: ${repoFetchError.message}`)
+        throw error(500, `Error fetching repo: ${repoFetchError.message}`)
+    }
+
+    if (!repo || !repo.project) {
+        return new Response(null, { status: 200 })
+    }
+
+    console.log('repo', repo)
+
+    const { data: deployment, error: deploymentInsertError } = await supabase
+        .from('deployment')
+        .insert({
+            repo_id: repo.id,
+            ref: requestJson.check_suite.head_branch,
+            sha: requestJson.check_suite.head_sha,
+            status: 'okay',
+            environment: 'dev',
+            url: 'something',
+        })
+        .select()
+        .single()
+
+    if (deploymentInsertError) {
+        console.log(`Error inserting deployment: ${deploymentInsertError.message}`)
+        throw error(500, `Error inserting deployment: ${deploymentInsertError.message}`)
+    }
+
+    console.log('deployment', deployment)
+
     return new Response('ok', { status: 200 })
 }
