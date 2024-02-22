c9cbd878fb940cd9a1beac4ab0dffb7b198fdd38
Isaac Harris-Holt <isaac@withpluto.com>
2024-02-22 11:50:15 +0000

    components: add tabbed code block

--- frontend/src/routes/dashboard/q/+page.svelte
+++ frontend/src/routes/dashboard/q/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import TabbedCodeBlock from '$lib/components/TabbedCodeBlock.svelte'
+</script>
+
 <div class="flex flex-col gap-12">
   <div class="card variant-soft-surface flex flex-col gap-4 p-8">
     <h2>Q</h2>
@@ -6,6 +10,31 @@
       by creating an <a href="/dashboard/api-keys" class="underline">API key</a>, then
       queue a message via the API.
     </p>
+    <TabbedCodeBlock
+      languages={[
+        {
+          code: `curl -X POST https://api.deplio.com/api/q -H 'X-Deplio-API-Key: <KEY>' -H '{"destination": "https://example.com", "method": "GET"}'`,
+          language: 'cURL',
+          lineNumbers: false,
+        },
+        {
+          code: `
+const response = await fetch('https://api.deplio.com/api/q', {
+  method: 'POST',
+  headers: {
+    'X-Deplio-API-Key': '<KEY>',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    destination: 'https://example.com',
+    method: 'GET',
+  })
+})`,
+          language: 'TypeScript',
+          lineNumbers: false,
+        },
+      ]}
+    />
   </div>
 
   <div class="card variant-soft-surface flex flex-col gap-4 p-8">
