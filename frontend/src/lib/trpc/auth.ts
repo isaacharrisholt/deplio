0f2af7b8f674e0abca30032494f8033855cb3f45
Isaac Harris-Holt <isaac@withpluto.com>
2024-02-23 17:09:43 +0000

    q: migrate post endpoint to trpc

--- frontend/src/lib/trpc/auth.ts
+++ frontend/src/lib/trpc/auth.ts
@@ -2,13 +2,14 @@ import { auth_schema } from '$lib/types/api/auth'
 import { getSupabaseAdminClient } from '$lib/utils.server'
 import { error } from '@sveltejs/kit'
 import { t } from './t'
+import { hashApiKey } from '$lib/apiKeys'
 
 const auth_middleware = t.middleware(async ({ input, ctx, next }) => {
   if (ctx.team) {
     return next({ ctx })
   }
 
-  const result = auth_schema.safeParse(input)
+  const result = auth_schema.required().safeParse(input)
   if (!result.success) {
     throw error(400, 'Invalid API key')
   }
@@ -17,13 +18,21 @@ const auth_middleware = t.middleware(async ({ input, ctx, next }) => {
 
   const supabase_admin = getSupabaseAdminClient()
 
+  const key_hash = await hashApiKey(api_key)
+  const key_prefix = api_key.slice(0, 6)
+
   const { data: stored_api_key, error: api_key_fetch_error } = await supabase_admin
     .from('api_key')
     .select('*, team(*)')
-    .eq('key', api_key)
+    .eq('key_hash', key_hash)
+    .eq('key_prefix', key_prefix)
+    .or(`expires_at.lte.${new Date().toISOString()}, expires_at.is.null`)
+    .is('revoked_at', null)
+    .is('deleted_at', null)
     .maybeSingle()
 
   if (api_key_fetch_error) {
+    console.error('error fetching api key', api_key_fetch_error)
     throw error(500, api_key_fetch_error.message)
   }
 
@@ -31,7 +40,11 @@ const auth_middleware = t.middleware(async ({ input, ctx, next }) => {
     throw error(401, 'Invalid API key')
   }
 
-  return await next({ ctx: { ...ctx, team: stored_api_key.team } })
+  return await next({
+    ctx: { ...ctx, team: stored_api_key.team, api_key: stored_api_key },
+  })
 })
 
-export const authenticated_procedure = t.procedure.use(auth_middleware)
+export const authenticated_procedure = t.procedure
+  .input(auth_schema)
+  .use(auth_middleware)
--- frontend/src/lib/trpc/context.ts
+++ frontend/src/lib/trpc/context.ts
@@ -1,4 +1,4 @@
-import type { Team } from '$lib/types/supabase'
+import type { APIKey, Team } from '$lib/types/supabase'
 import type { RequestEvent } from '@sveltejs/kit'
 
 export async function create_trpc_context<TEvent extends RequestEvent>(event: TEvent) {
@@ -6,6 +6,7 @@ export async function create_trpc_context<TEvent extends RequestEvent>(event: TE
     supabase: event.locals.supabase,
     user: event.locals.user,
     team: event.locals.team as Team,
+    api_key: undefined as APIKey | undefined,
   }
 }
 
