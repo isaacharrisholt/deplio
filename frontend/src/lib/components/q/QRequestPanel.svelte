5270163202710f01fe7a54857ac232be7747d1b2
Isaac Harris-Holt <isaac@withpluto.com>
2024-02-23 14:09:22 +0000

    q: add view button to qrequest panel

--- frontend/src/lib/components/q/QRequestPanel.svelte
+++ frontend/src/lib/components/q/QRequestPanel.svelte
@@ -3,6 +3,7 @@
   import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
   import QResponseStatus from './QResponseStatus.svelte'
   import CodeBlock from '../CodeBlock.svelte'
+  import { ArrowRight, Eye } from 'lucide-svelte'
 
   export let qRequest: QRequest
   // Even though we're only using the latest response, require all
@@ -16,11 +17,21 @@
 <div class="card">
   <Accordion>
     <AccordionItem>
-      <div slot="summary" class="flex flex-row items-center gap-4">
+      <div slot="summary" class="flex w-full flex-row items-center gap-4">
+        <!-- <a -->
+        <!--   href="/dashboard/q/requests/{qRequest.id}" -->
+        <!--   class="w-fit" -->
+        <!--   on:click={(e) => e.stopPropagation()} -->
+        <!--   on:keypress={(e) => e.stopPropagation()} -->
+        <!-- > -->
+        <!--   <button class="btn btn-sm variant-glass-primary"> -->
+        <!--     <Eye size={14} /> -->
+        <!--   </button> -->
+        <!-- </a> -->
         <QResponseStatus statusCode={latestResponse?.status_code} />
         <p class="font-mono text-sm">{qRequest.destination}</p>
       </div>
-      <div slot="content">
+      <div slot="content" class="flex flex-col gap-2 pb-2">
         {#if !latestResponse?.body}
           <p>Loading...</p>
         {:else}
