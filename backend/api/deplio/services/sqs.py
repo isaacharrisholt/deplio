557e81b740c13a9b2e6a96fca748d5ec1de39e70
Isaac Harris-Holt <isaac@withpluto.com>
2024-03-19 11:46:49 +0000

    ci: add terraform deployment

--- backend/api/deplio/services/sqs.py
+++ backend/api/deplio/services/sqs.py
@@ -3,8 +3,6 @@ import os
 import boto3
 from pydantic import BaseModel
 
-from deplio.types.enums import Environment
-
 
 class SQSMessage(BaseModel):
     Id: str
@@ -13,16 +11,13 @@ class SQSMessage(BaseModel):
 
 class SQS:
     def __init__(self: Self):
-        env = os.getenv('PUBLIC_DEPLOYMENT_ENV', Environment.LOCAL)
-        # TODO: Prod endpoint
-        endpoint = 'http://localhost:4566' if env == Environment.LOCAL else None
-        if endpoint is None:
-            raise NotImplementedError('Only local environment is supported by SQS')
+        endpoint = os.getenv('DEPLIO_Q_QUEUE_URL', 'http://localhost:4566')
+        region = os.getenv('AWS_REGION', 'eu-west-2')
 
         self.client = boto3.client(
             'sqs',
             endpoint_url=endpoint,
-            region_name='us-east-1',
+            region_name=region,
         )
 
     def list_queues(self: Self):
