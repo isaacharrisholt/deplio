344f11020c9c2242d3a5ad446d90aae22520fae7
isaacharrisholt <47423046+isaacharrisholt@users.noreply.github.com>
2024-05-03 10:51:02 +0000

    cron: add cron schedule validation (#113)

--- backend/api/deplio/models/data/v2024_02_26/endpoints/cron.py
+++ backend/api/deplio/models/data/v2024_02_26/endpoints/cron.py
@@ -2,12 +2,24 @@
 
 import typing
 import pydantic
-from typing import Optional
-from pydantic import BaseModel
+from typing import Annotated, Optional
+from cron_converter import Cron
+from fastapi.exceptions import RequestValidationError
+from pydantic import AfterValidator, BaseModel
 from deplio.models.data.head.db.cron import CronJobStatus, HTTPExecutor
+
+def validate_cron_schedule(schedule: str) -> str:
+    try:
+        c = Cron(schedule)
+        if c.parts is None:
+            raise RequestValidationError(['Invalid cron schedule: cron schedule is empty'])
+    except ValueError as e:
+        raise RequestValidationError([f'Invalid cron schedule: {e}']) from e
+    return schedule
+CronSchedule = Annotated[str, AfterValidator(validate_cron_schedule)]
 
 class PostCronJobRequest(BaseModel):
     status: CronJobStatus = CronJobStatus.ACTIVE
     executor: HTTPExecutor
-    schedule: str
+    schedule: CronSchedule
     metadata: Optional[dict[str, str]] = None
--- backend/api/deplio/routes/cron.py
+++ backend/api/deplio/routes/cron.py
@@ -1,6 +1,14 @@
+from typing import Annotated
+
+from cron_converter import Cron
+from fastapi import Depends
+from supabase._async.client import AsyncClient as SupabaseClient
+
+from deplio.context import Context, context
 from deplio.models.data.head.endpoints.cron import PostCronJobRequest
 from deplio.models.data.head.responses import generate_responses
 from deplio.routers import create_router
+from deplio.services.supabase import supabase_admin
 from deplio.tags import Tags
 
 router = create_router(prefix="/cron")
@@ -16,11 +24,13 @@ router = create_router(prefix="/cron")
 )
 async def create(
     # auth: Annotated[APIKeyAuthCredentials, Depends(api_key_auth)],
-    # supabase_admin: Annotated[SupabaseClient, Depends(supabase_admin)],
-    # context: Annotated[Context, Depends(context)],
+    supabase_admin: Annotated[SupabaseClient, Depends(supabase_admin)],
+    context: Annotated[Context, Depends(context)],
     # request: Request,
     cron_job: PostCronJobRequest,
 ):
-    print(cron_job)
+    print(cron_job, context)
+    cron_schedule = Cron(cron_job.schedule)
+    print(cron_schedule.parts)
 
     return cron_job
